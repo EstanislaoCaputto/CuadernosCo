9fc04b6df5ab9a203c593e1be8e71dd1855a17ec
Estanislao Caputto <caputto.estanislao@hotmail.com>
2021-07-17 16:37:13 -0300

    Primera entrega!

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          ¡Cuadernos artesanale!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/eclipsamesta/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Nuestro IG
         </a>
       </header>
     </div>
